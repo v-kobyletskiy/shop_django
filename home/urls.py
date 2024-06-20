bc2b1ececaa30af0a78276a1ae89649e82848c82
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-06-20 16:51:53 +0300

    add routing

--- home/urls.py
+++ home/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from home import views
+
+app_name = 'home'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home')),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
