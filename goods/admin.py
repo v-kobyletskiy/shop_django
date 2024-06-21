ef0d0894be8b322d43d89a3f5117bd1534aa5808
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-06-21 22:20:01 +0300

    add 'products' models

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from goods.models import ProductCategory, Product
 
-# Register your models here.
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'name', 'slug', 'position', 'is_visible')
+    list_editable = ('name', 'position', 'is_visible')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('photo_src_tag', 'name', 'slug', 'description', 'photo', 'price', 'photo', 'price', 'discount',
+                    'quantity', 'position', 'is_visible')
+    list_editable = ('name', 'position', 'is_visible')
+    list_filter = ('category', 'is_visible')
+    search_fields = ('name',)
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width=50 height=50>")
+
+    photo_src_tag.short_description = 'Product photo'
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    position = models.SmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name_plural = 'Product Categories'
+
+class Product(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+    slug = models.SlugField(max_length=75, unique=True)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='products/', blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    discount = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    position = models.SmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name_plural = 'Products'
