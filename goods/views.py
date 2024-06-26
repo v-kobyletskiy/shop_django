ddab0a817208f58e3da50a3de0a337624d79f944
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-06-26 15:29:50 +0300

    Обратное разрешение url адресов

--- goods/views.py
+++ goods/views.py
@@ -15,5 +15,9 @@ def catalog(request):
     return render(request, 'catalog.html', context)
 
 
-def product(request):
-    return render(request, 'product.html')
+def product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'product.html', context=context)
