57fe765563cd7f595eb63b19b0d8e6627152b281
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-06-28 15:03:45 +0300

    add filtering products

--- goods/views.py
+++ goods/views.py
@@ -9,12 +9,19 @@ CATEGORY_ALL = 'all'
 
 def catalog(request, category_slug):
     page = request.GET.get('page', 1)
+    order_by = request.GET.get('order_by', None)
+    on_sale = request.GET.get('on_sale', None)
 
     if category_slug == CATEGORY_ALL:
         goods = Product.objects.all()
     else:
         # goods = Product.objects.filter(category__slug=category_slug)
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
