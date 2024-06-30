92161e24f0d5cc39075fd3f78ee2328a20ff5717
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-06-30 14:14:21 +0300

    'search' by name, description

--- goods/utils.py
+++ goods/utils.py
@@ -1,9 +1,18 @@
+from django.db.models import Q
 from goods.models import Product
 
 
 def query_search(query):
     if is_id(query):
         return Product.objects.filter(id=int(query))
+    keywords = [word for word in query.split() if len(word) > 3]
+
+    queries = Q()
+
+    for keyword in keywords:
+        queries |= Q(name__icontains=keyword)
+        queries |= Q(description__icontains=keyword)
+    return Product.objects.filter(queries)
 
 
 def is_id(search_param):
