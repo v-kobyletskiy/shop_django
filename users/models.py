c2b5edb7893c951b76ec77447dbc0c24fc7559fe
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-02 14:33:39 +0300

    override 'User' model for registration user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Avatar')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
