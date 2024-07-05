43aff0282f344d68f7bbdd2fa4d156a19ae62507
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-05 21:36:35 +0300

    add 'login' from

--- users/forms.py
+++ users/forms.py
@@ -5,17 +5,21 @@ from .models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label='Username',
-        widget=forms.TextInput(attrs={'autofocus': True,
-                                      'class': 'form-control',
-                                      'placeholder': 'Enter your username'}))
-    password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                          'class': 'form-control',
-                                          'placeholder': 'Enter your password'}),
-    )
+    username = forms.CharField()
+    password = forms.CharField()
+
     class Meta:
         model = User
         fields = ('username', 'password')
+
+    # username = forms.CharField(
+    #     label='Username',
+    #     widget=forms.TextInput(attrs={'autofocus': True,
+    #                                   'class': 'form-control',
+    #                                   'placeholder': 'Enter your username'}))
+    # password = forms.CharField(
+    #     label='Password',
+    #     widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
+    #                                       'class': 'form-control',
+    #                                       'placeholder': 'Enter your password'}),
+    # )
