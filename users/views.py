d0cb2c6efb08373a2f420d4dd72a98422774bb4c
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-09 18:47:45 +0300

    redirect to 'next' parameter page (unauth profile-login-profile)

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, you are now logged in')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('home:home'))
     else:
         form = UserLoginForm()
