177da1a97917990965e2f1709171c377332ab75b
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-18 22:06:46 +0300

    add 'remove' product

--- carts/views.py
+++ carts/views.py
@@ -18,8 +18,11 @@ def cart_add(request, product_slug):
 
     return redirect(request.META['HTTP_REFERER'])  # return to previous page
 
-def cart_remove(request, product_slug):
-    ...
+
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
 
 
 def cart_change(request, product_slug):
