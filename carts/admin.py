1254bf5e432e95aa4ccc4b7ce16819a76eeca221
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-19 17:26:06 +0300

    in 'user' admin show his carts

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Cart
 
+
+class CartTabAdmin(admin.TabularInline):
+    model = Cart
+    fields = ('product', 'quantity', 'created_at')
+    search_fields = ('product', 'quantity', 'created_at')
+    readonly_fields = ('created_at',)
+    extra = 1
+
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'quantity', 'created_at')
--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 from .models import User
+from carts.admin import CartTabAdmin
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email']
     search_fields = ['username', 'first_name', 'last_name', 'email']
+
+    inlines = [CartTabAdmin,]
