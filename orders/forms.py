9682ff3711d15e82fa7f03e3ae4e3dc22edb155b
Vasyl Kobyletskyi <v.kobyletskiy@viseven.com>
2024-07-23 15:48:50 +0300

    add order from (WIP)

--- orders/forms.py
+++ orders/forms.py
@@ -1,29 +1,38 @@
 from django import forms
 
 class CreateOrderForm(forms.Form):
-    first_name = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
-    )
-    last_name = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
-    )
-    phone_number = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'})
-    )
-    requires_delivery = forms.ChoiceField(
-        widget=forms.RadioSelect(),
-        choices=[('0', False), ('1', True)],
-        initial=0,
-    )
-    delivery_address = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'class': 'form-control',
-            'id': 'delivery-address',
-            'rows': 2,
-            'placeholder': 'Delivery Address'})
-    )
-    payment_on_get = forms.ChoiceField(
-        widget=forms.RadioSelect(),
-        choices=[('0', False), ('1', True)],
-        initial='card',
-    )
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    phone_number = forms.CharField()
+    requires_delivery = forms.ChoiceField()
+    delivery_address = forms.CharField(required=False)
+    payment_on_get = forms.ChoiceField()
+
+
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    # )
+    # phone_number = forms.CharField(
+    #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'})
+    # )
+    # requires_delivery = forms.ChoiceField(
+    #     widget=forms.RadioSelect(),
+    #     choices=[('0', False), ('1', True)],
+    #     initial=0,
+    # )
+    # delivery_address = forms.CharField(
+    #     widget=forms.Textarea(attrs={
+    #         'class': 'form-control',
+    #         'id': 'delivery-address',
+    #         'rows': 2,
+    #         'placeholder': 'Delivery Address'})
+    # )
+    # payment_on_get = forms.ChoiceField(
+    #     widget=forms.RadioSelect(),
+    #     choices=[('0', False), ('1', True)],
+    #     initial='card',
+    # )
